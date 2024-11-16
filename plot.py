3c34d88b84d2be3569bc69abd0f822ff9ee58690
stefanpatrichi <stefanpatrichi07@gmail.com>
2024-11-16 20:53:48 +0200

    add file

---
+++ plot.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import math as np
+from decimal import *
+
+getcontext().prec = 30
+
+n = int(input())
+a = Decimal(input())
+b = Decimal(input())
+
+def musso_up_to(n, a, b):
+    u = [a]
+    v = [b]
+    for i in range(n):
+        u.append((u[-1] + v[-1]) / Decimal(2))
+        v.append(Decimal(u[-1] * v[-1]).sqrt())
+    return [u, v]
+
+def agm_up_to(n, a, b):
+    u = [b]
+    v = [a]
+    for i in range(n):
+        u.append((u[-1] + v[-1]) / Decimal(2))
+        v.append(Decimal(u[-2] * v[-1]).sqrt())
+    return [u, v]
+
+u, v = agm_up_to(n, a, b)
+
+def diffs(n, u, v):
+    d = []
+    for i in range(1, n + 1):
+        d.append((v[i] - u[i]) / (v[i - 1] - u[i - 1]))
+    return d
+
+d = diffs(n - 1, u, v)
+print(d)
+
+# ------------------------------------------------------
+
+x = []
+for i in range(1, len(u) + 1):
+    x.append(i)
+
+plt.plot(x, u, '-')
+plt.plot(x, v, '-')
+plt.show()
