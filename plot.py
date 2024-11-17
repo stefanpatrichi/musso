7627022e2a15ef90b3084885921f694e3ee7d05d
stefanpatrichi <stefanpatrichi07@gmail.com>
2024-11-17 15:38:46 +0200

    convert to sympy and add convergence rate and order

--- plot.py
+++ plot.py
@@ -1,43 +1,70 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from decimal import *
+from sympy import *
 
-getcontext().prec = 30
+PRECISION = 50
+
+def sym_to_arr(arr):
+    retval = []
+    for x in arr:
+        retval.append(N(x, PRECISION))
+    return retval
+
+def musso_lim(a, b):
+    return b * sin(acos(a / b)) / acos(a/b)
 
 def musso_up_to(n, a, b):
-    u = np.array([a])
-    v = np.array([b])
+    u = [a]
+    v = [b]
     for i in range(n):
-        u = np.append(u, (u[-1] + v[-1]) / 2)
-        v = np.append(v, np.sqrt(u[-1] * v[-1]))
-    return [u, v]
+        u.append((u[-1] + v[-1]) / 2)
+        v.append(sqrt(u[-1] * v[-1]))
+    return [Array(u), Array(v)]
 
 def agm_up_to(n, a, b):
-    u = np.array([b])
-    v = np.array([a])
+    u = [b]
+    v = [a]
     for i in range(n):
-        u = np.append(u, (u[-1] + v[-1]) / 2)
-        v = np.append(v, np.sqrt(u[-2] * v[-1]))
-    return [u, v]
-
+        u.append((u[-1] + v[-1]) / 2)
+        v.append(sqrt(u[-2] * v[-1]))
+    return [Array(u), Array(v)]
 
 def diffs(n, u, v):
-    d = np.array([])
+    d = []
     for i in range(n):
-        d = np.append(d, (v[i + 1] - u[i + 1]) / (v[i] - u[i]))
-    return d
+        d.append(Abs(v[i + 1] - u[i + 1]) / Abs(v[i] - u[i]))
+    return Array(d)
+
+def conv_order(n, v, lim):
+    p = []
+    for i in range(n):
+        p.append(log(Abs(v[i + 1] - lim)) / log(Abs(v[i] - lim)))
+    return Array(p)
+
+def conv_rate(n, v, lim, ord):
+    p = []
+    for i in range(n):
+        p.append(Abs(v[i + 1] - lim) / Abs(v[i] - lim)**ord)
+    return Array(p)
 
 
 # ------------------------------------------------------
 
 n = int(input())
-a = float(input())
-b = float(input())
+a = sympify(input())
+b = sympify(input())
 
-u, v = agm_up_to(n, a, b)
+if a > b:
+    a, b = b, a
 
-d = diffs(n - 1, u, v)
-print(d)
+_, v = musso_up_to(n, a, b)
+# print(sym_to_arr(u))
+# d = diffs(n - 1, u, v)
+# p = conv_order(n - 2, v, v[n - 1])
+# print(v)
+r = conv_rate(n - 1, v, musso_lim(a, b), 1)
+print(sym_to_arr(r))
+# print(sym_to_arr(p))
 
 # x = []
 # for i in range(1, len(u) + 1):
