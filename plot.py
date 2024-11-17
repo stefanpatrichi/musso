56fc86dca64148d498bd1bacf5131f1b977a193c
stefanpatrichi <stefanpatrichi07@gmail.com>
2024-11-17 13:23:57 +0200

    convert to numpy

--- plot.py
+++ plot.py
@@ -1,49 +1,49 @@
 import matplotlib.pyplot as plt
-import math as np
+import numpy as np
 from decimal import *
 
 getcontext().prec = 30
 
 def musso_up_to(n, a, b):
-    u = [a]
-    v = [b]
+    u = np.array([a])
+    v = np.array([b])
     for i in range(n):
-        u.append((u[-1] + v[-1]) / Decimal(2))
-        v.append(Decimal(u[-1] * v[-1]).sqrt())
+        u = np.append(u, (u[-1] + v[-1]) / 2)
+        v = np.append(v, np.sqrt(u[-1] * v[-1]))
     return [u, v]
 
 def agm_up_to(n, a, b):
-    u = [b]
-    v = [a]
+    u = np.array([b])
+    v = np.array([a])
     for i in range(n):
-        u.append((u[-1] + v[-1]) / Decimal(2))
-        v.append(Decimal(u[-2] * v[-1]).sqrt())
+        u = np.append(u, (u[-1] + v[-1]) / 2)
+        v = np.append(v, np.sqrt(u[-2] * v[-1]))
     return [u, v]
 
 
 def diffs(n, u, v):
-    d = []
-    for i in range(1, n + 1):
-        d.append((v[i] - u[i]) / (v[i - 1] - u[i - 1]))
+    d = np.array([])
+    for i in range(n):
+        d = np.append(d, (v[i + 1] - u[i + 1]) / (v[i] - u[i]))
     return d
 
 
 # ------------------------------------------------------
 
 n = int(input())
-a = Decimal(input())
-b = Decimal(input())
+a = float(input())
+b = float(input())
 
 u, v = agm_up_to(n, a, b)
 
 d = diffs(n - 1, u, v)
 print(d)
 
-x = []
-for i in range(1, len(u) + 1):
-    x.append(i)
+# x = []
+# for i in range(1, len(u) + 1):
+#     x.append(i)
 
-plt.plot(x, u, '-')
-plt.plot(x, v, '-')
-plt.legend()
-plt.show()
+# plt.plot(x, u, '-')
+# plt.plot(x, v, '-')
+# plt.legend()
+# plt.show()
