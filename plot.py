348c294ba9b203036370cbec21a8d1c3d9717bde
stefanpatrichi <stefanpatrichi07@gmail.com>
2024-11-22 21:23:41 +0200

    alternate convergence rate function

--- plot.py
+++ plot.py
@@ -47,17 +47,13 @@ def archimedean_iteration(n, mean1, mean2, a, b):
     return [u, v]
 
 def apm_up_to(n, a, b):
-    if a > b:
-        a, b = b, a
     return archimedean_iteration(n, am, gm, a, b)
 
 def asm_up_to(n, a, b):
-    if a > b:
-        a, b = b, a
-    return archimedean_iteration(n, gm, am, a, b)
+    return archimedean_iteration(n, gm, am, b, a)
 
 def agm_up_to(n, a, b):
-    return gaussian_iteration(n, am, gm, a, b)
+    return gaussian_iteration(n, gm, am, a, b)
 
 def ham_up_to(n, a, b):
     return gaussian_iteration(n, hm, am, a, b)
@@ -83,36 +79,39 @@ def conv_rate(n, v, lim, ord):
         mu.append(Abs(v[i + 1] - lim) / Abs(v[i] - lim)**ord)
     return mu
 
+def conv_rate2(n, u, v, ord):
+    mu = []
+    for i in range(n):
+        mu.append(Abs(v[i + 1] - u[i + 1]) / Abs(v[i] - u[i])**ord)
+    return mu
+
 # ------------------------------------------------------
 
 n = int(input())
 a = sympify(input())
 b = sympify(input())
 
-if a > b:
-    a, b = b, a
-
-u, v = gqm_up_to(2, a, b)
-print(N(v[1]))
-print(N(am(a, b)))
-# up, vp   = asm_up_to(n, a, b)
-# u, v = agm_up_to(n, a, b)
-# print(sym_to_arr(up))
-# print(sym_to_arr(u))
+u, v     = apm_up_to(n, a, b)
+up, vp   = asm_up_to(n, a, b)
+upp, vpp = agm_up_to(n, a, b)
+print(sym_to_arr(upp))
+print(sym_to_arr(conv_rate2(n - 2, u, v, 1)))
+print(sym_to_arr(conv_rate2(n - 2, up, vp, 1)))
+print(sym_to_arr(conv_rate(n - 2, vpp, upp[n - 1], 2)))
 # d = diffs(n - 1, u, v)
 # p = conv_order(n - 2, v, v[n - 1])
 # print(v)
 # print(sym_to_arr(test(n, v, musso_lim(a, b))))
 
-# x = []
-# for i in range(1, len(upp) + 1):
-#     x.append(i)
+x = []
+for i in range(1, len(upp) + 1):
+    x.append(i)
 
-# plt.plot(x, u, '-', label="APM u")
-# plt.plot(x, v, '-', label="APM v")
-# plt.plot(x, up, '-', label="ASM u")
-# plt.plot(x, vp, '-', label="ASM v")
-# plt.plot(x, upp, '-', label="AGM u")
-# plt.plot(x, vpp, '-', label="AGM v")
-# plt.legend()
-# plt.show()
+plt.plot(x, u, '-', label="APM u")
+plt.plot(x, v, '-', label="APM v")
+plt.plot(x, up, '-', label="ASM u")
+plt.plot(x, vp, '-', label="ASM v")
+plt.plot(x, upp, '-', label="AGM u")
+plt.plot(x, vpp, '-', label="AGM v")
+plt.legend()
+plt.show()
